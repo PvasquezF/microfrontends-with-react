887a0eaed2d7c4e65e36eb234943dc890ff70640
Pavel <alexanderpavelv32@gmail.com>
2021-06-09 15:58:56 -0600

    Estableciendo configuraciones para generar los build de produccion

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,5 +1,4 @@
 const { merge } = require("webpack-merge");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const commonConfig = require("./webpack.common");
 const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 const packageJson = require("../package.json");
@@ -19,10 +18,7 @@ const devConfig = {
         marketing: "marketing@http://localhost:8081/remoteEntry.js",
       },
       shared: packageJson.dependencies,
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
+    })
   ],
 };
 

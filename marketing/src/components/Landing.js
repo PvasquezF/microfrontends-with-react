aaebf4bd052ed67fea197d115396b752668f962f
Pavel <alexanderpavelv32@gmail.com>
2021-06-09 16:55:47 -0600

    actualizando landing

--- marketing/src/components/Landing.js
+++ marketing/src/components/Landing.js
@@ -91,6 +91,16 @@ export default function Album() {
               contents, the creator, etc. Make it short and sweet, but not too
               short so folks don&apos;t simply skip over it entirely.
             </Typography>
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph
+            >
+              Something short and leading about the collection below—its
+              contents, the creator, etc. Make it short and sweet, but not too
+              short so folks don&apos;t simply skip over it entirely.
+            </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
                 <Grid item>

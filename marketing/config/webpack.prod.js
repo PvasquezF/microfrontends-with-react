2824930167c9864e0dcb6bf1ae4846a22305feb2
Pavel <alexanderpavelv32@gmail.com>
2021-06-10 08:08:47 -0600

    Adding marketing microfronted deployment

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -6,6 +6,7 @@ const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
+    publicPath: "/marketing/latest/"
   },
   plugins: [
     new ModuleFederationPlugin({

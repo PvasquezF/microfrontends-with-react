2696b20f8d070840681cb3ae590f44a7a6e098e2
Pavel <alexanderpavelv32@gmail.com>
2021-06-10 13:05:56 -0600

    deploying auth microfrontend

--- container/src/app.js
+++ container/src/app.js
@@ -20,7 +20,10 @@ export default () => {
     <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
-          <Header isSignedIn={isSignedIn}/>
+          <Header
+            onSignOut={() => setIsSignedIn(false)}
+            isSignedIn={isSignedIn}
+          />
           <Suspense fallback={<Progress />}>
             <Switch>
               <Route path="/auth">

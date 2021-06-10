510923f1cc1c8978f55914997587b0ecb0b0c946
Pavel <alexanderpavelv32@gmail.com>
2021-06-10 08:51:56 -0600

    Adding header component

--- container/src/app.js
+++ container/src/app.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
 
 export default () => {
   return (
-    <div>
-      <h1>Container</h1>
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
